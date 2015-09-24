d8b038db9215844a66f4d75a75bc1297c1d9581c
jheliot <spyladder@gmail.com>
2015-09-24 17:15:43 +0200

    Ajout de tests sur la copie de variables

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
-print "Hello Spyladder"
+print ("Hello Spyladder")
 dico = {}
 dico["nom"] = "heliot"
 dico["prenom"] = "julien"
 
-print dico
+print (dico)
+
+a = 1
+b = a
+print(a, b)
+
+a = 3
+print(a, b)
