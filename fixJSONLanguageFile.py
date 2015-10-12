6b6177ef88ecea27667cec53961c1613645e5d05
spyladder <spyladder@gmail.com>
2015-10-12 19:16:33 +0200

    Removed unused code. Get path automaticaly. Save json fixed file in another file.

--- fixJSONLanguageFile.py
+++ fixJSONLanguageFile.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+import os
 import json
 
-BASE_DIR = "C:\\WORKSPACE\\RunMyMail_Trunk\\WebApps\\browser\\htmls\\REF\\Localization\\"
+BASE_DIR = os.getcwd()
 # fileName = "localizables.en_US"
 # fileName = "localizables.test"
-fileName = "localizables.en_US_ori"
-inputFile = open(BASE_DIR + "\\" + fileName + ".json", "r")
+# fileName = "localizables.en_US_ori"
+fileName = "localizables.en_US_bad"
+inputFile = open(BASE_DIR + "/" + fileName + ".json", "r")
 
-# jsonStr = inputFile.read()
-# print(json.loads(jsonStr))
 try:
 	jsonObj = json.load(inputFile)
-	print(type(jsonObj))
-	print(jsonObj)
+	with open(BASE_DIR + "/" + fileName + "_m.json", "w") as outputFile:
+		json.dump(jsonObj, outputFile, sort_keys = True, indent = 4)
 except ValueError as e:
 	print(e)
 
-
-#for line in inputFile:
-
-
 inputFile.close()
-
-
-# dico = {}
-# dico["nom"] = "heliot"
-# dico["prenom"] = "julien"
-
-# print (dico)
-
-# a = 1
-# b = a
-# print(a, b)
-
-# a = 3
-# print(a, b)
