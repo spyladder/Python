525bdb8c5dd94971389f0c4bc3869283aff8d9dc
spyladder <spyladder@gmail.com>
2015-12-04 17:15:01 +0100

    Add a filedialog to select a directory (the flash repository).

--- FlashManager/flashManager.py
+++ FlashManager/flashManager.py
@@ -2,10 +2,20 @@
 # -*- coding: utf-8 -*-
  
 from tkinter import * 
+from tkinter import filedialog
 
 fenetre = Tk()
 
 label = Label(fenetre, text="Hello World")
 label.pack()
+
+repertoire = filedialog.askdirectory(
+	parent = fenetre,
+	initialdir = "/",
+	title = 'Choisissez un repertoire')
+
+if len(repertoire) > 0:
+    print ("vous avez choisi le repertoire %s" % repertoire)
+
 
 fenetre.mainloop()
